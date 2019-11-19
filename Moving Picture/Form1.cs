116cf62bde6e6f233cd5f828cb6935ba66513290
Holly Carr <rc0037728@student.riversidecollege.ac.uk>
2019-11-19 11:45:54 +0000

    Made direction buttons so picture can move, up down left and right.

--- Moving Picture/Form1.cs
+++ Moving Picture/Form1.cs
@@ -19,12 +19,12 @@ namespace Moving_Picture
 
         private void Button1_Click(object sender, EventArgs e)
         {
-            pictureBox1.Location = new Point(pictureBox1.Location.X, pictureBox1.Location.Y + 5);
+            pictureBox1.Location = new Point(pictureBox1.Location.X, pictureBox1.Location.Y - 5);
         }
 
         private void Button2_Click(object sender, EventArgs e)
         {
-            pictureBox1.Location = new Point(pictureBox1.Location.X, pictureBox1.Location.Y -5);
+            pictureBox1.Location = new Point(pictureBox1.Location.X, pictureBox1.Location.Y + 5);
         }
 
         private void Button3_Click(object sender, EventArgs e)
